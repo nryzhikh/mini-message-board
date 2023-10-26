4ddefe892aecc25d9c0853d8e928e0e7b195ae3b
nryzhikh <nikita.ryzhikh@outlook.com>
2023-10-26 21:37:01 +0400

    Complete mini-message-board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var messages = require('../messages');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Mini Messageboard", messages: messages },);
 });
 
 module.exports = router;
---
+++ routes/new.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var messages = require('../messages');
+
+router.post('/', (req, res) => {
+  const text = req.body.messages;
+    const user = req.body.user;
+    messages.push({text: text, user: user, added: new Date()});
+    res.redirect('/');
+
+});
+
+module.exports = router;
